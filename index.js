4d72c1c712e180d059fa348b4f6bd1bfeb592c1c
mahiuddinhabib <mh.habib137@gmail.com>
2022-05-23 23:51:06 +0600

    setting up project

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from tools')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
