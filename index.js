42b8ff6d2a54b1bfb2221475fa2eea5f2fe22793
mahiuddinhabib <mh.habib137@gmail.com>
2022-05-26 02:33:22 +0600

    make admin

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { use } = require('express/lib/application');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,20 +15,20 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// function verifyJWT(req, res, next) {
-//     const authHeaders = req.headers.authorization;
-//     if (!authHeaders) {
-//         return res.status(401).send({ message: 'unauthorized access' });
-//     }
-//     const token = authHeaders.split(' ')[1];
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-//         if (err) {
-//             return res.status(403).send({ message: 'Forbidden access' });
-//         }
-//         req.decoded = decoded;
-//         next();
-//     });
-// }
+function verifyJWT(req, res, next) {
+    const authHeaders = req.headers.authorization;
+    if (!authHeaders) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeaders.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 
 async function run() {
     try {
@@ -70,12 +71,18 @@ async function run() {
             res.send(tool);
         });
 
-        app.get('/my-orders', async (req, res) => {
+        app.get('/my-orders', verifyJWT, async (req, res) => {
             const email = req.query.email;
-            const query = { email };
-            const cursor = ordersCollection.find(query);
-            const myOrders = await cursor.toArray();
-            res.send(myOrders);
+            const decodedEmail = req.decoded.email;
+            if(email===decodedEmail){
+                const query = { email };
+                const cursor = ordersCollection.find(query);
+                const myOrders = await cursor.toArray();
+                res.send(myOrders);
+            }
+            else{
+                return res.status(403).send({ message: 'forbidden' });
+            }
         });
 
         app.delete('/my-orders/:id', async (req, res) => {
@@ -97,6 +104,23 @@ async function run() {
             res.send(user);
         });
 
+        app.put('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await usersCollection.findOne({email: requester});
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+            }
+            else{
+                res.status(403).send({message: 'forbidden access'})
+            }
+        });
+
         app.put('/users/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
@@ -108,6 +132,13 @@ async function run() {
             const result = await usersCollection.updateOne(filter, updatedDoc, options);
             const token = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
             res.send({ result, token });
+        })
+
+        app.get('/admin/:email', async(req, res)=>{
+            const email = req.params.email;
+            const user = await usersCollection.findOne({email:email})
+            const isAdmin = user.role==='admin';
+            res.send({admin:isAdmin});
         })
     }
     finally {
